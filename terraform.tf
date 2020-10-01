663bb0ed126053413f8fc26de7215ab886cbc866
Barbosa <Marcelo.Barbosa@arvato.com>
2020-10-01 23:35:45 +0300

    removed backend terraform status

--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,10 @@
 terraform {
   required_version = "~> 0.13"
 
-  backend "s3" {
-    bucket               = "aws-sample-terraform-state"
-    key                  = "tf-state.json"
-    region               = "us-east-1"
-    workspace_key_prefix = "environment"
-  }
+  #backend "s3" {
+  #  bucket               = "aws-sample-terraform-state"
+  #  key                  = "tf-state.json"
+  #  region               = "us-east-1"
+  #  workspace_key_prefix = "environment"
+  #}
 }
